95bf3da2502bd5509989e73701939394f423a7b4
Merna Gamal <merna.gamal.shenouda@gmail.com>
2024-01-04 19:03:12 +0200

    Lab3_3_JS

--- Lab3-JS/Part3/Ex1.js
+++ Lab3-JS/Part3/Ex1.js
@@ -2,35 +2,42 @@
 //Use 10 of Objects predefined methods and explain them
 
 //1-Object.keys():
+//This method returns an array of a given object's own enumerable property names.
 const obj1 = { a: 1, b: 2, c: 3 };
 console.log(Object.keys(obj1)); // Output: ['a', 'b', 'c']
 
 //2-Object.values():
+//It returns an array of a given object's own enumerable property values.
 const obj2 = { a: 1, b: 2, c: 3 };
 console.log(Object.values(obj2)); // Output: [1, 2, 3]
 
 //3-Object.entries():
+//This method returns an array of a given object's own enumerable property [key, value] pairs.
 const obj3 = { a: 1, b: 2, c: 3 };
 console.log(Object.entries(obj3)); // Output: [['a', 1], ['b', 2], ['c', 3]]
 
 //4-Object.assign():
+//It copies the values of all enumerable own properties from one or more source objects to a target object and returns the target object.
 const objx = { a: 1 };
 const objy = { b: 2 };
 const mergedObj = Object.assign({}, objx, objy);
 console.log(mergedObj); // Output: { a: 1, b: 2 }
 
 //5-Object.hasOwnProperty():
+//Checks if a specific property is an own property of an object and returns a boolean value.
 const obj5 = { a: 1, b: 2 };
 console.log(obj5.hasOwnProperty('a')); // Output: true
 console.log(obj5.hasOwnProperty('c')); // Output: false
 
 //6-Object.freeze():
+//It freezes an object, preventing new properties from being added to it, existing properties from being removed, or property descriptors from being changed.
 const obj6 = { a: 1 };
 Object.freeze(obj6);
 // Trying to modify the frozen object
 obj6.b = 2; // Ignored in strict mode; will not add 'b' to the object
 
 //7-Object.seal():
+//It seals an object, preventing new properties from being added or existing properties from being removed, but allows changing the values of existing properties.
 const obj7 = { a: 1 };
 Object.seal(obj7);
 // Trying to add/remove properties
@@ -38,11 +45,13 @@ obj7.b = 2; // Ignored in strict mode; will not add 'b' to the object
 delete obj7.a; // Ignored in strict mode; will not delete 'a' from the object
 
 //8-Object.fromEntries():
+//This method transforms a list of key-value pairs into an object.
 const entries = [['a', 1], ['b', 2], ['c', 3]];
 const obj8 = Object.fromEntries(entries);
 console.log(obj8); // Output: { a: 1, b: 2, c: 3 }
 
 //9-Object.getPrototypeOf():
+//This method returns the prototype of the specified object.
 const person1 = {
     name: 'Alice',
     age: 30,
@@ -51,6 +60,7 @@ const prototypeOfPerson = Object.getPrototypeOf(person1);
 console.log(prototypeOfPerson); // Output: {} (default object prototype)
 
 //10-Object.getOwnPropertyNames():
+//Returns an array of all properties (including non-enumerable properties except for symbols) found directly in a given object.
 const person2 = {
     name: 'Alice',
     age: 30,
--- Lab3-JS/Part3/Ex2.js
+++ Lab3-JS/Part3/Ex2.js
@@ -2,54 +2,64 @@
 //Use 10 of Arrays predefined methods and explain them.
 
 //1-Array.push():
+//The push() method adds one or more elements to the end of an array and returns the new length of the array.
 const array1 = [1, 2, 3];
 array1.push(4, 5);
 console.log(array1); // Output: [1, 2, 3, 4, 5]
 
 //2-Array.pop():
+//The pop() method removes the last element from an array and returns that element.
 const array2 = [1, 2, 3];
 const removedElement = array2.pop();
 console.log(removedElement); // Output: 3
 console.log(array2); // Output: [1, 2]
 
 //3-Array.unshift():
+//The unshift() method adds one or more elements to the beginning of an array and returns the new length of the array.
 const array3 = [2, 3];
 array3.unshift(0, 1);
 console.log(array3); // Output: [0, 1, 2, 3]
 
 //4-Array.shift():
+//The shift() method removes the first element from an array and returns that element.
 const array4 = [0, 1, 2, 3];
 const remElement = array4.shift();
 console.log(remElement); // Output: 0
 console.log(array4); // Output: [1, 2, 3]
 
 //5-Array.slice():
+//The slice() method returns a shallow copy of a portion of an array into a new array object.
 const array5 = [1, 2, 3, 4, 5];
 const newArray = array5.slice(1, 4);
 console.log(newArray); // Output: [2, 3, 4]
 
 //6-Array.splice():
+//The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place.
 const array6 = [1, 2, 3, 4, 5];
 array6.splice(2, 1, 'a', 'b');
 console.log(array6); // Output: [1, 2, 'a', 'b', 4, 5]
 
 //7-Array.concat():
+//The concat() method is used to merge two or more arrays, returning a new array without modifying the existing arrays.
 const array_x = [1, 2];
 const array_y = [3, 4];
 const newAr = array_x.concat(array_y);
 console.log(newAr); // Output: [1, 2, 3, 4]
 
 //8-Array.includes():
+//The includes() method determines whether an array includes a certain element, returning true or false as appropriate.
 const array8 = [1, 2, 3];
 console.log(array8.includes(2)); // Output: true
 console.log(array8.includes(4)); // Output: false
 
 //9-Array.indexOf():
+//The indexOf() method returns the first index at which a given element can be found in the array, or -1 if it is not present.
 const array9 = [1, 2, 3, 2];
 console.log(array9.indexOf(2)); // Output: 1
 console.log(array9.indexOf(4)); // Output: -1
 
 //10-Array.join():
+//The join() method creates and returns a new string by concatenating all the elements in an array (or an array-like object), separated by a specified separator.
 const array10 = ['Hello', 'World', '!'];
 const joinedString = array10.join(' ');
 console.log(joinedString); // Output: 'Hello World !'
